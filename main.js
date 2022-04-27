870685b5bb8a8573401c6cb00e6582d69ab5d271
arshia <arshiabdol@yahoo.com>
2022-04-27 15:54:40 -0400

    added not working merge sort to be fixed

--- main.js
+++ main.js
@@ -153,6 +153,65 @@
 
 
 
+
+    //mergeSort stack over flows on browser to be implemented when i find a fix
+    const merge = function(arr, left, middle, right){
+        let n1 = middle - left + 1;
+        let n2 = right - middle;
+
+
+        let leftArr = new Array(n1);
+        let rightArr = new Array(n2);
+
+
+        for(let i = 0; i < n1; i++){//copy the data to l and r arrays
+            leftArr[i] = arr[l+i];
+        }
+        for(let j = 0; j < n2; j++){
+            rightArr[j] = arr[middle + 1 + j];
+        }
+
+        let i = 0, j = 0;//reseting indees of subarrays
+        let k = l;
+
+        while(i < n1 && j < n2){
+
+            if(leftArr[i] <= rightArr[j]){
+                arr[k] = leftArr[i];
+                i++;
+            }else{
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+
+        }
+
+
+
+        while(i < n1){
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        while(j < n2){
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+
+    }
+    const mergeSort = function(arr, left, right){
+        if(left >= right){
+            return;
+        }
+
+        let middle = left + parseInt((right-1)/2);
+        mergeSort(arr,left,right)
+        mergeSort(arr,middle+1,right)
+        merge(arr,left,middle,right)
+    }
+    
     //qucik sort
     const qucikSortSwapHelper = function(arr,x,y){
         
